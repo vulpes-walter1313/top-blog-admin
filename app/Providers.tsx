cf5c0f567ad6cca2ebd377c08ed6f0a8ffa74069
Walter Fritsche <wfritsche95@protonmail.com>
2024-02-14 10:20:18 -0600

    added useQuery setup

---
+++ app/Providers.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useState } from "react";
+import type React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+function Providers({
+  children,
+}: {
+  children: React.ReactNode;
+}): React.JSX.Element {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false}/>
+      </QueryClientProvider>
+  );
+}
+
+export default Providers;
