6756b32c5e6b64fb029e4032592bafdb40fdeb7d
Walter Fritsche <wfritsche95@protonmail.com>
2024-03-14 10:27:48 -0500

    added create post button on top nav when logged in

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -45,9 +45,17 @@ function Navbar() {
               <Link href="/posts">Posts</Link>
             </li>
             <li>
+              <Link
+                href="/posts/create"
+                className="rounded-md bg-indigo-600 px-6 py-2 font-semibold text-zinc-50"
+              >
+                Create Post
+              </Link>
+            </li>
+            <li>
               <button
                 type="button"
-                className="rounded-md bg-indigo-600 px-6 py-2 font-semibold text-zinc-50"
+                className="rounded-md border-2 border-indigo-600 px-6 py-1 font-semibold text-indigo-600"
                 onClick={() => logoutMutation.mutate()}
               >
                 Logout
